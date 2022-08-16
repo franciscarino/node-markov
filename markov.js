f5fa48f82f688b07de65ad2c6f64807bb2951322
Francis Carino <fgcarino@gmail.com>
2022-08-16 16:29:19 -0700

    update per code review

--- markov.js
+++ markov.js
@@ -1,8 +1,7 @@
+"use strict";
 /** Textual markov chain generator. */
 
-
 class MarkovMachine {
-
   /** Build markov machine; read in text.*/
 
   constructor(text) {
@@ -15,7 +14,7 @@ class MarkovMachine {
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,22 +22,50 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
-  }
+    let wordMap = new Map();
 
+    // loop over words and make key in map or push to key array
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1];
+
+      if (nextWord === undefined) {
+        nextWord = null;
+      }
+
+      if (wordMap.has(word)) {
+        //push next word onto key
+        wordMap.get(word).push(nextWord);
+      } else {
+        wordMap.set(word, [nextWord]);
+      }
+    }
+    return wordMap;
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
   getText() {
-    // TODO: implement this!
+    let sentence = this.words[0];
+    let currentWord = this.words[0];
 
-    // - start at the first word in the input text
-    // - find a random word from the following-words of that
-    // - repeat until reaching the terminal null
+    while (currentWord !== null) {
+      let wordArray = this.chains.get(currentWord);
+      let idx = Math.floor(Math.random() * wordArray.length);
+
+      // avoid adding "null" to sentence.
+      if (wordArray[idx] !== null) {
+        sentence += ` ${wordArray[idx]}`;
+      }
+
+      //set next word to current word
+      currentWord = wordArray[idx];
+    }
+    return sentence;
   }
 }
